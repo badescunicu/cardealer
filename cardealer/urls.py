cf54968944bdd41af1b70605e0531ccf295d8977
badescunicu <badescunicu@yahoo.com>
2013-03-24 17:28:08 +0200

    Add views.py, index.html template, bootstrap.css stylesheet

--- cardealer/urls.py
+++ cardealer/urls.py
@@ -6,17 +6,20 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'cardealer.views.home', name='home'),
     # url(r'^cardealer/', include('cardealer.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
+    
+    
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'cardealer.views.home', name='home'),
 )
 
-urlpatterns += patterns('',
-    url(r'^resources/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT}),
-)
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^resources/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    )
