ab128277b2a5f892ea081506aec9d61b235ab1ff
badescunicu <badescunicu@yahoo.com>
2013-03-24 17:53:41 +0200

    Add list_cars.html

--- cardealer/urls.py
+++ cardealer/urls.py
@@ -16,6 +16,7 @@ urlpatterns = patterns('',
     
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'cardealer.views.home', name='home'),
+    url(r'^cars/$', 'cardealer.views.list_all_cars', name='list_all_cars'),
 )
 
 if settings.DEBUG:
--- cardealer/views.py
+++ cardealer/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render_to_response
+from cardealer.models import Manufacturer, Car
 
 def home(request):
     return render_to_response('index.html')
+
+def list_all_cars(request):
+    title = 'Available cars'
+    cars = Car.objects.all()
+    #manufacturers = Manufacturer.objects.all()
+    context = {
+        #'manufacturers': manufacturers,
+        'cars': cars,
+        'title': title
+    }
+    return render_to_response('list_cars.html', context)
