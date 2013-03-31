ce8555d6e217b820ac7792a562fedfe6e9d88b8c
Nicu Badescu <badescunicu@yahoo.com>
2013-03-31 16:03:59 +0300

    Use generic class based views

--- cardealer/urls.py
+++ cardealer/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from cardealer.views import ListCars
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,7 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'cardealer.views.home', name='home'),
-    url(r'^cars/$', 'cardealer.views.list_all_cars', name='list_all_cars'),
+#    url(r'^cars/$', 'cardealer.views.list_all_cars', name='list_all_cars'),
+    url(r'^cars/$', ListCars.as_view()),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/', include('api.urls')),
 )
--- cardealer/views.py
+++ cardealer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from cardealer.models import Manufacturer, Car
+from django.views.generic import ListView
 from django.http import Http404
 
 def home(request):
@@ -13,3 +14,13 @@ def list_all_cars(request):
         'title': title,
     }
     return render_to_response('list_cars.html', context)
+
+
+class ListCars(ListView):
+    model = Car
+    template_name = 'list_cars.html'
+    context_object_name = 'cars'
+    
+    
+    
+    
